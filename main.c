b86d6b333598ad1c678211d5e81cae3e18f84f57
christislord12 <53404933+christislord12@users.noreply.github.com>
2019-09-21 14:28:52 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(int argc, const char * argv[]) {
+    printf("For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.\n");
+    return 0;
+}
